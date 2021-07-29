81284be95658ea197d84c5601bbf6fb04d10e069
riyanris <riyanrisky129@gmail.com>
2021-07-29 14:27:35 +0700

    add mail msg

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -68,6 +68,145 @@ class BaseController extends Controller
 		helper(["form", "ini_helper"]);
 	}
 
+	function getTableUsers()
+	{
+		$jenis = $this->session->get('jenis');
+		$bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+
+		$body = "<table class=\"table table-bordered\" width=\"100%\">";
+
+		$body .= "<tr>";
+		$body .= "<td>Nama</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('nama')."</td>";
+		$body .= "</tr>";
+
+		$body .= "<tr>";
+		$body .= "<td>Email</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('email')."</td>";
+		$body .= "</tr>";
+
+		$body .= "<tr>";
+		$body .= "<td>Tempat, Tanggal Lahir</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('tempat').", ".$this->session->get('tanggal')." ".$bulan[($this->session->get('bulan') - 1)]." ".$this->session->get('tahun')."</td>";
+		$body .= "</tr>";
+
+		$body .= "<tr>";
+		$body .= "<td>Jenis Kelamin</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('jenkel')."</td>";
+		$body .= "</tr>";
+
+		$body .= "<tr>";
+		$body .= "<td>Agama</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('agama')."</td>";
+		$body .= "</tr>";
+
+		$body .= "<tr>";
+		$body .= "<td>No. Handphone</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('nohp')."</td>";
+		$body .= "</tr>";
+
+		$body .= "<tr>";
+		$body .= "<td>Status Keanggotaan</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('jenis')."</td>";
+		$body .= "</tr>";
+
+		if($jenis == "Pelajar"){
+
+			$body .= "<tr>";
+			$body .= "<td>Nama Sekolah</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('sekolah')."</td>";
+			$body .= "</tr>";
+
+		}elseif($jenis == "Mahasiswa"){
+
+			$body .= "<tr>";
+			$body .= "<td>Universitas</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('univ')."</td>";
+			$body .= "</td>";
+
+			$body .= "<tr>";
+			$body .= "<td>Jenjang</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('jenjang')."</td>";
+			$body .= "</tr>";
+
+			$body .= "<tr>";
+			$body .= "<td>Jurusan</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('jurusan')."</td>";
+			$body .= "</tr>";
+
+			$body .= "<tr>";
+			$body .= "<td>Status Perkawinan</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('perkawinan')."</td>";
+			$body .= "</tr>";
+
+		}elseif($jenis == "Masyarakat" || $jenis == "Alumni"){
+
+			$body .= "<tr>";
+			$body .= "<td>Perusahaan</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('perusahaan')."</td>";
+			$body .= "</td>";
+
+			$body .= "<tr>";
+			$body .= "<td>Jabatan</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('jabatan')."</td>";
+			$body .= "</tr>";
+
+			$body .= "<tr>";
+			$body .= "<td>Status Perkawinan</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$this->session->get('perkawinan')."</td>";
+			$body .= "</tr>";
+
+		}
+
+		if($jenis != "Masyarakat"){
+
+			$alamat = $this->session->get('ajalan').". kel. ".$this->session->get('akel').", kec. ".$this->session->get('akec').". ".$this->session->get('akod');
+
+			$body .= "<tr>";
+			$body .= "<td>Alamat di Aceh Tenggara</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$alamat."</td>";
+			$body .= "</tr>";
+
+		}
+
+		if($jenis != "Alumni"){
+
+			$alamat = $this->session->get('djalan').". Kel. ".$this->session->get('dkel').", kec. ".$this->session->get('dkec').", ".$this->session->get('dkab').". ".$this->session->get('dkod');
+
+			$body .= "<tr>";
+			$body .= "<td>Alamat di DIY</td>";
+			$body .= "<td> : </td>";
+			$body .= "<td>".$alamat."</td>";
+			$body .= "</tr>";
+
+		}
+
+		$body .= "<tr>";
+		$body .= "<td>Password</td>";
+		$body .= "<td> : </td>";
+		$body .= "<td>".$this->session->get('pw')."</td>";
+		$body .= "</tr>";
+
+		$body .= "</table>";
+		return $body;
+	}
+
 	function mailer($to, $title, $body)
 	{
 		$mail = new PHPMailer;
--- app/Controllers/Pendataan.php
+++ app/Controllers/Pendataan.php
@@ -108,142 +108,7 @@ class Pendataan extends BaseController
     }else{
       $this->session->set($this->request->getPost());
 
-      $jenis = $this->session->get('jenis');
-
-      $bulan = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
-
-      $body = "<table class=\"table table-bordered\" width=\"100%\">";
-
-      $body .= "<tr>";
-      $body .= "<td>Nama</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('nama')."</td>";
-      $body .= "</tr>";
-
-      $body .= "<tr>";
-      $body .= "<td>Email</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('email')."</td>";
-      $body .= "</tr>";
-
-      $body .= "<tr>";
-      $body .= "<td>Tempat, Tanggal Lahir</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('tempat').", ".$this->session->get('tanggal')." ".$bulan[($this->session->get('bulan') - 1)]." ".$this->session->get('tahun')."</td>";
-      $body .= "</tr>";
-
-      $body .= "<tr>";
-      $body .= "<td>Jenis Kelamin</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('jenkel')."</td>";
-      $body .= "</tr>";
-
-      $body .= "<tr>";
-      $body .= "<td>Agama</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('agama')."</td>";
-      $body .= "</tr>";
-
-      $body .= "<tr>";
-      $body .= "<td>No. Handphone</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('nohp')."</td>";
-      $body .= "</tr>";
-
-      $body .= "<tr>";
-      $body .= "<td>Status Keanggotaan</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('jenis')."</td>";
-      $body .= "</tr>";
-
-      if($jenis == "Pelajar"){
-
-        $body .= "<tr>";
-        $body .= "<td>Nama Sekolah</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('sekolah')."</td>";
-        $body .= "</tr>";
-
-      }elseif($jenis == "Mahasiswa"){
-
-        $body .= "<tr>";
-        $body .= "<td>Universitas</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('univ')."</td>";
-        $body .= "</td>";
-
-        $body .= "<tr>";
-        $body .= "<td>Jenjang</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('jenjang')."</td>";
-        $body .= "</tr>";
-
-        $body .= "<tr>";
-        $body .= "<td>Jurusan</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('jurusan')."</td>";
-        $body .= "</tr>";
-
-        $body .= "<tr>";
-        $body .= "<td>Status Perkawinan</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('perkawinan')."</td>";
-        $body .= "</tr>";
-
-      }elseif($jenis == "Masyarakat" || $jenis == "Alumni"){
-
-        $body .= "<tr>";
-        $body .= "<td>Perusahaan</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('perusahaan')."</td>";
-        $body .= "</td>";
-
-        $body .= "<tr>";
-        $body .= "<td>Jabatan</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('jabatan')."</td>";
-        $body .= "</tr>";
-
-        $body .= "<tr>";
-        $body .= "<td>Status Perkawinan</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$this->session->get('perkawinan')."</td>";
-        $body .= "</tr>";
-
-      }
-
-      if($jenis != "Masyarakat"){
-
-        $alamat = $this->session->get('ajalan').". kel. ".$this->session->get('akel').", kec. ".$this->session->get('akec').". ".$this->session->get('akod');
-
-        $body .= "<tr>";
-        $body .= "<td>Alamat di Aceh Tenggara</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$alamat."</td>";
-        $body .= "</tr>";
-
-      }
-
-      if($jenis != "Alumni"){
-
-        $alamat = $this->session->get('djalan').". Kel. ".$this->session->get('dkel').", kec. ".$this->session->get('dkec').", ".$this->session->get('dkab').". ".$this->session->get('dkod');
-
-        $body .= "<tr>";
-        $body .= "<td>Alamat di DIY</td>";
-        $body .= "<td> : </td>";
-        $body .= "<td>".$alamat."</td>";
-        $body .= "</tr>";
-
-      }
-
-      $body .= "<tr>";
-      $body .= "<td>Password</td>";
-      $body .= "<td> : </td>";
-      $body .= "<td>".$this->session->get('pw')."</td>";
-      $body .= "</tr>";
-
-      $body .= "</table>";
-
+      $body = $this->getTableUsers();
     }
 
     $data = [
@@ -288,7 +153,21 @@ class Pendataan extends BaseController
     ];
     $lastid = $users->simpan($additionalData);
     if($lastid){
+
+      $to = $this->session->get('email');
+
+      $title = "Pendaftaran Ikamara Yogyakarta Berhasil";
+      
+      $body = "Halo ".$this->session->get('nama').". Terima kasih telah melakukan pendaftaran di <a href=\"".base_url()."\">Sistem Pendataan Anggota Ikamara Yogyakarta</a>. Berikut data yang berhasil kami simpan: <br/>";
+      $body .= $this->getTableUsers();
+      $body .= "<br/><hr/><br/><p>Jika ini kesalahan atau kamu tidak merasa melakukan pendaftaran ini, silahkan hubungi kami di alamat email <a href=\"mailto:cs@ikamara.org\">cs@ikamara.org<a>.</p>";
+      $body .= "<p>Atas perhatianya kami sampaikan terima kasih.</p><br/>";
+      $body .= "<p>Salam hormat, admin <a href=\"".base_url()."\">Ikamara Yogyakarta</a>.</p>";
+
+      $mail = $this->mailer($to, $title, $body);
+      
       $this->clearSession();
+      
       return redirect()->to(site_url("pendataan/berhasil"))->with("msg_success", "Berhasil menyimpan data");
     }else{
       echo "Error: Tidak dapat menyimpan data, harap ulangi beberapa saat lagi.";
